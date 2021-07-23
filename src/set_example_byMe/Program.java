2565232e477ecf57af6512969b34ec8e6fbb3222
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-07-23 08:42:16 -0300

    Finished example

--- src/set_example_byMe/Program.java
+++ src/set_example_byMe/Program.java
@@ -1,9 +1,12 @@
 package set_example_byMe;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.TreeSet;
+
 
 public class Program {
 
@@ -14,6 +17,26 @@ public class Program {
 
 		Set<Employee> set = new TreeSet<Employee>();
 		
+		System.out.println("Number emps: ");
+		int numberEmps = scan.nextInt();
+		try {
+			for (int i = 0; i < numberEmps; i++) {
+			System.out.println("-INFO-");
+			System.out.print("Name: ");
+			String name = scan.next();
+			System.out.print("Birth (dd/MM/yyyy): ");
+			Date birth = sdf.parse(scan.next());
+			System.out.print("Salary: ");
+			Double salary = scan.nextDouble();
+			
+			set.add(new Employee(name, birth, salary));
+			}
+			for (Employee emp : set) {
+				System.out.println(emp);
+			}
+		}
+		catch (ParseException e) {
+			System.out.println(e.getMessage());
+		}
 	}
-
 }
