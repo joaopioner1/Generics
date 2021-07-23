667325d8d8104f0907fadea2072e7c063b1f485e
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-07-23 08:31:34 -0300

    Scanner, set and sdf are ready

--- src/set_example_byMe/Program.java
+++ src/set_example_byMe/Program.java
@@ -1,10 +1,18 @@
 package set_example_byMe;
 
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Program {
 
 	public static void main(String[] args) {
 		//Author: Joao Vitor Souza Pioner Date: 23/07/2021
-		
+		Scanner scan = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+		Set<Employee> set = new TreeSet<Employee>();
 		
 	}
 
