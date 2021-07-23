7215219bc21b482680bd9ecdceb602a7c7247c92
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-07-23 08:22:42 -0300

    Finished class Employee

--- src/set_example_byMe/Employee.java
+++ src/set_example_byMe/Employee.java
@@ -1,5 +1,59 @@
 package set_example_byMe;
 
-public class Employee {
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+public class Employee implements Comparable<Employee>{
+	
+	static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private String name;
+	private Date birth;
+	private Double salary;
+	
+	public Employee() {
+	}
+
+	public Employee(String name, Date birth, Double salary) {
+		this.name = name;
+		this.birth = birth;
+		this.salary = salary;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Date getBirth() {
+		return birth;
+	}
+
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+
+	public Double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(Double salary) {
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "EMPLOYEE DATA\n" 
+				+ "Name: " + getName()
+				+ "\nBirth date: " + sdf.format(getBirth())
+				+ "\nSalary: " + String.format("%.2f", getSalary()) + "\n-------------------";
+	}
+	
+	@Override 
+	public int compareTo(Employee other) {
+		return getBirth().compareTo(other.getBirth());
+	}
 }
