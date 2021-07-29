7ad5594f806a24f49839245a9a85224c770ecf1a
Joao Vitor Pioner <joaopioner123@gmail.com>
2021-07-28 21:31:49 -0300

    add exercise (error)

--- src/exercicio_fixacao/Program.java
+++ src/exercicio_fixacao/Program.java
@@ -1,6 +1,8 @@
 package exercicio_fixacao;
 
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class Program {
 
@@ -8,7 +10,39 @@ public class Program {
 		// Author: Joao Vitor Souza Pioner | Date: 28/07/2021
 		Scanner scan = new Scanner(System.in);
 		
+		Integer code=0;
+		Set<User> set = new TreeSet<>();
 		
+		System.out.print("How many students for course A?");
+		Integer quantityA_students = scan.nextInt();
+		
+		for (int i = 0; i < quantityA_students; i++) {
+			code = scan.nextInt();
+			if (!set.contains(new User(code))) {
+				set.add(new User(code));
+			}
+		}
+		
+		System.out.print("How many students for course B?");
+		Integer quantityB_students = scan.nextInt();
+		
+		for (int i = 0; i < quantityB_students; i++) {
+			code = scan.nextInt();
+			if (!set.contains(new User(code))) {
+				set.add(new User(code));
+			}
+		}
+		
+		System.out.print("How many students for course C?");
+		Integer quantityC_students = scan.nextInt();
+		
+		for (int i = 0; i < quantityC_students; i++) {
+			code = scan.nextInt();
+			if (!set.contains(new User(code))) {
+				set.add(new User(code));
+			}
+		}
+		System.out.println(set.size());
 	}
 
 }
